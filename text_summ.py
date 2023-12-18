ef02dcac3638012d3ec4c6d26ad0e02f26fa34ba
Ankit <raazankit1502@gmail.com>
2023-12-18 13:34:33 +0530

    feat:updated

--- text_summ.py
+++ text_summ.py
@@ -11,47 +11,62 @@ def extract_text_from_pdf(pdf_path):
     return text
 
 
-pdf_path = "C:/farewell/rabbit.pdf"
+'''pdf_path = "C:/farewell/rabbit.pdf"
 pdf_text = extract_text_from_pdf(pdf_path)
-print(pdf_text)
+print(pdf_text)'''
 
 
-def generate_summary_and_sentiment(input_text):
+def generate_summary_and_sentiment(input_text, chunk_size=512):
+    # Split the input text into chunks of specified size
+    chunks = [input_text[i:i + chunk_size] for i in range(0, len(input_text), chunk_size)]
+
+    # Initialize variables to store results
+    combined_summary = ''
+    average_sentiment_score = 0.0
+
     # Sentiment analysis pipeline
     sentiment_analyzer = pipeline('sentiment-analysis', model='nlptown/bert-base-multilingual-uncased-sentiment')
-    
-
-    # Analyze sentiment of the input text
-    sentiment_result = sentiment_analyzer(input_text)[0]
-    sentiment_label = sentiment_result['label']
-    sentiment_score = sentiment_result['score']
 
     # Choose a Text Summarization Model
     model_name = "t5-small"
     tokenizer = T5Tokenizer.from_pretrained(model_name)
     model = T5ForConditionalGeneration.from_pretrained(model_name)
 
-    # Tokenize the input text
-    tokenized_input = tokenizer.encode("summarize: " + input_text, return_tensors="pt", max_length=512, truncation=True)
+    # Analyze sentiment and generate summary for each chunk
+    for chunk in chunks:
+        # Analyze sentiment of the current chunk
+        sentiment_result = sentiment_analyzer(chunk)[0]
+        sentiment_score = sentiment_result['score']
+        average_sentiment_score += sentiment_score
 
-    # Generate Summarization
-    summary_ids = model.generate(tokenized_input, max_length=150, length_penalty=2.0, num_beams=4, early_stopping=True)
-    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+        # Tokenize the current chunk
+        tokenized_input = tokenizer.encode("summarize: " + chunk, return_tensors="pt", max_length=512, truncation=True)
 
-    return summary, sentiment_label, sentiment_score
+        # Generate Summarization for the current chunk
+        summary_ids = model.generate(tokenized_input, max_length=150, length_penalty=2.0, num_beams=4, early_stopping=True)
+        summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
+        # Combine summaries from different chunks
+        combined_summary += summary
+
+    # Calculate overall sentiment based on the average score
+    average_sentiment_score /= len(chunks)
+    overall_sentiment_label = 'positive' if average_sentiment_score >= 0.5 else 'negative'
+
+    return combined_summary, overall_sentiment_label, average_sentiment_score
 
 
 
 
-pdf_path = "C:/farewell/rabbit.pdf"
-pdf_text = extract_text_from_pdf(pdf_path)
-summary, sentiment_label, sentiment_score = generate_summary_and_sentiment(pdf_text)
+#pdf_path = "C:/farewell/rabbit.pdf"
+#pdf_text = extract_text_from_pdf(pdf_path)
+#summary, sentiment_label, sentiment_score = generate_summary_and_sentiment(pdf_text)
 #print(f"Summary: {summary}\nSentiment: {sentiment_label} (Score: {sentiment_score:.2f})")
 
 
 
 
-from gtts import gTTS
+'''from gtts import gTTS
 import os
 language = 'en'
 
@@ -62,4 +77,4 @@ tts = gTTS(text=summary, lang=language, slow=False)
 tts.save("output.mp3")
 
 # Playing the converted file
-os.system("start output.mp3")
+os.system("start output.mp3")'''
